5574ded3341769ec11b7e6bca84d0ba112cd4cef
Jo Roe <jroe@invoca.com>
2022-01-12 12:34:30 -0800

    Day 7 part one all tests pass

--- lib/day7.rb
+++ lib/day7.rb
@@ -5,4 +5,23 @@ class Day7
     raw = File.read(file)
     raw.split(',').map(&:to_i)
   end
+
+  def self.fuel_for_best_pos(crabs)
+    fuel_totals = []
+    for i in 0 .. crabs.size - 1 do
+      fuel_totals << fuel_for_pos(crabs, i)
+    end
+    fuel_totals.min
+  end
+
+  def self.fuel_for_pos(crabs, position)
+    fuel_total = 0
+    for i in 0 .. crabs.size - 1 do
+      fuel_total += (crabs[i] - position).abs
+    end
+    fuel_total
+  end
 end
+
+#crabs = Day7.load_data('../data/JoDay7.txt')
+#puts Day7.fuel_for_best_pos(crabs)
--- spec/day7_spec.rb
+++ spec/day7_spec.rb
@@ -3,8 +3,21 @@ require_relative '../lib/day7'
 
 describe 'Day 7' do
   context 'Part 1' do
+    crabs = Day7.load_data('data/day7example.txt')
+
     it 'crab positions are read into array' do
-      expect(Day7.load_data('data/day7example.txt')).to eq([16,1,2,0,4,2,7,1,2,14])
+      expect(crabs).to eq([16,1,2,0,4,2,7,1,2,14])
+    end
+
+    it 'calculates fuel used to move to a specific position' do
+      expect(Day7.fuel_for_pos(crabs, 2)).to eq(37)
+      expect(Day7.fuel_for_pos(crabs, 1)).to eq(41)
+      expect(Day7.fuel_for_pos(crabs, 3)).to eq(39)
+      expect(Day7.fuel_for_pos(crabs, 10)).to eq(71)
+    end
+
+    it 'position that uses minimum fuel uses 37' do
+      expect(Day7.fuel_for_best_pos(crabs)).to eq(37)
     end
   end
 end
