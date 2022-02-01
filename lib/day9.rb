7afb1ca14d704a52f8ae197e629d894e5189f2d7
Jo Roe <jroe@invoca.com>
2022-01-31 16:53:06 -0800

    solution for Day 9 part 1

--- lib/day9.rb
+++ lib/day9.rb
@@ -4,6 +4,120 @@ class Day9
   def self.load_data(file)
     raw = File.readlines(file).map(&:strip)
 
-    raw.each { |line| line.each_char.map(&:to_i) }
+    output = []
+
+    raw.each do |line|
+      output_line = []
+      line.each_char do |char|
+        output_line << char.to_i
+      end
+      output << output_line
+    end
+
+    output
+  end
+
+  def self.sum_risk_level(data)
+    find_lowest_points(data).map { |depth| depth + 1 }.sum
+  end
+
+  def self.find_top_low_points(data, rows, cols)
+    low_points = []
+    #top left
+    point = data[0][0]
+    if point < data[0][1] && point < data[1][0]
+      low_points << point
+    end
+
+    #top right
+    point = data[0][cols-1]
+    if point < data[0][cols-2] && point < data[1][cols-1]
+      low_points << point
+    end
+
+    for i in 1 .. cols - 2
+      point = data[0][i]
+      if point < data[0][i-1] && point < data[0][i+1] && point < data[1][i]
+        low_points << point
+      end
+    end
+
+    low_points
+  end
+
+  def self.find_bottom_low_points(data, rows, cols)
+    low_points = []
+    # bottom left
+    point = data[rows-1][0]
+    if point < data[rows-2][0] && point < data[rows-1][1]
+      low_points << point
+    end
+
+    #bottom right
+    point = data[rows-1][cols-1]
+    if point < data[rows-1][cols-2] && point < data[rows-2][cols-1]
+      low_points << point
+    end
+
+    for i in 1 .. cols - 2
+      point = data[rows-1][i]
+      if point < data[rows-1][i-1] && point < data[rows-1][i+1] && point < data[rows-2][i]
+        low_points << point
+      end
+    end
+
+    low_points
+  end
+
+  def self.find_side_low_points(data, rows, cols)
+    low_points = []
+    for i in 1 .. rows - 2
+      #left side (not first or last)
+      point = data[i][0]
+
+      if point < data[i][1] && point < data[i+1][0] && point < data[i-1][0]
+        low_points << point
+      end
+
+      #right side (not first or last)
+      point = data[i][cols-1]
+
+      if point < data[i][cols-2] && point < data[i+1][cols-1] && point < data[i-1][cols-1]
+        low_points << point
+      end
+    end
+
+    low_points
+  end
+
+  def self.find_middle_low_points(data, rows, cols)
+    low_points = []
+    for i in 1 .. rows - 2
+      for j in 1 .. cols - 2
+        point = data[i][j]
+        if point < data[i][j-1] && point < data[i][j+1] && point < data[i-1][j] && point < data[i+1][j]
+          low_points << point
+        end
+      end
+    end
+    low_points
+  end
+
+  def self.find_lowest_points(data)
+    rows = data.size
+    cols = data[0].size
+
+    low_points = []
+
+    low_points += find_top_low_points(data, rows, cols)
+
+    #last row
+    low_points += find_bottom_low_points(data, rows, cols)
+
+    #middles
+    low_points += find_side_low_points(data, rows, cols)
+    low_points += find_middle_low_points(data, rows, cols)
   end
 end
+
+puts Day9.sum_risk_level(Day9.load_data('../data/JoDay9.txt'))
--- spec/day9_spec.rb
+++ spec/day9_spec.rb
@@ -7,6 +7,27 @@ describe 'day 9' do
     it 'loads points into 2D array' do
       expect(input.size).to eq(5)
       expect(input[0].size).to eq(10)
+      expect(input[0][0].class).to eq(Integer)
+    end
+
+    it 'low points in top row' do
+      expect(Day9.find_top_low_points(input, 5, 10)).to match_array([1, 0])
+    end
+
+    it 'low points in bottom row' do
+      expect(Day9.find_bottom_low_points(input, 5, 10)).to match_array([5])
+    end
+
+    it 'low points on sides' do
+      expect(Day9.find_side_low_points(input, 5, 10)).to match_array([])
+    end
+
+    it 'low points middles' do
+      expect(Day9.find_middle_low_points(input, 5, 10)).to match_array([5])
+    end
+
+    it 'sum of risk levels of all low points' do
+      expect(Day9.sum_risk_level(input)).to eq(15)
     end
   end
 end
